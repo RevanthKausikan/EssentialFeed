f2ee98385b839e16f6c239a4ac56450d728d1d65
Revanth Kausikan <revanth.kausikan@freshworks.com>
2025-02-24 18:33:13 +0530

    Initial Commit

---
+++ EssentialFeedTests/EssentialFeedTests.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeedTests.swift
+//  EssentialFeedTests
+//
+//  Created by Revanth Kausikan on 24/02/25.
+//
+
+import Testing
+@testable import EssentialFeed
+
+struct EssentialFeedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
