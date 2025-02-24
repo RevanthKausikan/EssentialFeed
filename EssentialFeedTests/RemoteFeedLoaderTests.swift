5b766d62b9fe01984d66febebda94e792f04da44
RevanthKausikan <revanth.k.h@gmail.com>
2025-02-24 19:23:52 +0530

    load_requestsDataFromURL

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,21 +9,38 @@ import Testing
 @testable import EssentialFeed
 
 struct RemoteFeedLoader {
-    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "www.any-url.com")
+    }
 }
 
-struct HTTPClient {
+final class HTTPClient {
+    static let shared = HTTPClient()
+    
+    private init() { }
+    
     var requestedURL: URL?
 }
 
 
 struct RemoteFeedLoaderTests {
 
-    @Test func init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+    @Test
+    func init_doesNotRequestDataFromURL() {
+        let client = HTTPClient.shared
         let _ = RemoteFeedLoader()
         
         #expect(client.requestedURL == nil)
+    }
+    
+    @Test
+    func load_requestsDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        
+        sut.load()
+        
+        #expect(client.requestedURL != nil)
     }
 
 }
