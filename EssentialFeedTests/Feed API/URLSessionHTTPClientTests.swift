89ab80743dc5d43b15259aa4c703854b17fdf6c8
RevanthKausikan <revanth.k.h@gmail.com>
2025-02-26 16:55:27 +0530

    url session task call count test

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -18,31 +18,60 @@ final class URLSessionHTTPClient {
     
     func get(from url: URL) {
         let urlRequest = URLRequest(url: url)
-        session.dataTask(with: urlRequest) { _, _, _ in }
+        session.dataTask(with: urlRequest, completionHandler: { _, _, _ in }).resume()
     }
 }
 
 struct URLSessionHTTPClientTests {
-    @Test("Get requests from URL - captures data task with URL")
+    @Test("Get requests from URL - Create - captures data task with URL")
     func test_getFromURL_createsDataTaskWithURL() {
         let url = URL(string: "any-url.com")!
         let session = URLSessionSpy()
-        
         let sut = URLSessionHTTPClient(session: session)
+        
         sut.get(from: url)
         
         #expect(session.receivedURLs == [url])
+    }
+    
+    @Test("Get requests from URL - Resume - captures data task with URL")
+    func test_getFromURL_resumesDataTaskWithURL() {
+        let url = URL(string: "any-url.com")!
+        let task = URLSessionDataTaskSpy()
+        let session = URLSessionSpy()
+        session.stub(url, with: task)
+        let sut = URLSessionHTTPClient(session: session)
+        
+        sut.get(from: url)
+        
+        #expect(task.resumeCallCount == 1)
     }
 }
 
 // MARK: - Helpers
 fileprivate final class URLSessionSpy: URLSession, @unchecked Sendable {
     var receivedURLs = [URL]()
+    private var stubs = [URL: URLSessionDataTask]()
+    
+    func stub(_ url: URL, with task: URLSessionDataTask) {
+        stubs[url] = task
+    }
     
     override func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, (any Error)?) -> Void) -> URLSessionDataTask {
-        receivedURLs.append(request.url!)
-        return FakeURLSessionDataTask()
+        let url = request.url!
+        receivedURLs.append(url)
+        return stubs[url] ?? FakeURLSessionDataTask()
     }
 }
 
-fileprivate final class FakeURLSessionDataTask: URLSessionDataTask, @unchecked Sendable { }
+fileprivate final class FakeURLSessionDataTask: URLSessionDataTask, @unchecked Sendable {
+    override func resume() { }
+}
+
+fileprivate final class URLSessionDataTaskSpy: URLSessionDataTask, @unchecked Sendable {
+    var resumeCallCount = 0
+    
+    override func resume() {
+        resumeCallCount += 1
+    }
+}
