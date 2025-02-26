d0df6be94babdc3a5219a1ece5a9de3385708c3c
RevanthKausikan <revanth.k.h@gmail.com>
2025-02-26 21:03:54 +0530

    moved URLSessionHTTPClient to its own file

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -9,28 +9,6 @@ import Testing
 import Foundation
 import EssentialFeed
 
-final class URLSessionHTTPClient: HTTPClient {
-    private let session: URLSession
-    
-    init(session: URLSession = .shared) {
-        self.session = session
-    }
-    
-    struct UnexpectedValuesRepresentation: Error { }
-    
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-        session.dataTask(with: url) { data, response, error in
-            if let error {
-                completion(.failure(error))
-            } else if let data, let response = response as? HTTPURLResponse {
-                completion(.success(data, response))
-            } else {
-                completion(.failure(UnexpectedValuesRepresentation()))
-            }
-        }.resume()
-    }
-}
-
 @Suite(.serialized)
 final class URLSessionHTTPClientTests: EFTesting {
     
