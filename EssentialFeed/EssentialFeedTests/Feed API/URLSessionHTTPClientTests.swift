f58bf36f4f0bd40cf854e79ef18d4aeecb36d0c6
RevanthKausikan <revanth.k.h@gmail.com>
2025-03-04 23:20:16 +0530

    added thread sanitizer

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -178,7 +178,6 @@ fileprivate final class URLProtocolStub: URLProtocol {
     }
     
     override class func canInit(with request: URLRequest) -> Bool {
-        requestObserver?(request)
         return true
     }
     
@@ -187,6 +186,11 @@ fileprivate final class URLProtocolStub: URLProtocol {
     }
     
     override func startLoading() {
+        if let requestObserver = URLProtocolStub.requestObserver {
+            client?.urlProtocolDidFinishLoading(self)
+            return requestObserver(request)
+        }
+        
         if let data = URLProtocolStub.stub?.data {
             client?.urlProtocol(self, didLoad: data)
         }
