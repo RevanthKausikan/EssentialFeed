9e5bf754b92bf3d564194f61231857f247358fc3
RevanthKausikan <revanth.k.h@gmail.com>
2025-03-07 19:33:52 +0530

    Save requests cache deletion

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,13 +6,27 @@
 //
 
 import Testing
+import Foundation
+import EssentialFeed
 
-struct FeedStore {
+final class FeedStore {
     var deleteCachedFeedCallCount = 0
+    
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
 
 struct LocalFeedLoader {
     let store: FeedStore
+    
+    init(store: FeedStore) {
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
+    }
 }
 
 struct CacheFeedUseCaseTests {
@@ -24,5 +38,24 @@ struct CacheFeedUseCaseTests {
         
         #expect(store.deleteCachedFeedCallCount == 0)
     }
+    
+    @Test("Save requests cache deletion")
+    func save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        
+        let items = [uniqueItem, uniqueItem]
+        sut.save(items)
+        
+        #expect(store.deleteCachedFeedCallCount == 1)
+    }
+}
 
+// MARK: - Helpers
+extension CacheFeedUseCaseTests {
+    private var uniqueItem: FeedItem {
+        .init(id: UUID(), description: "any", location: "any", imageURL: anyURL)
+    }
+    
+    private var anyURL: URL { URL(string: "any-url.com")! }
 }
