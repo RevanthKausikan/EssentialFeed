0c52b00f5356b62bdaafd3014890793194b5aae5
RevanthKausikan <revanth.k.h@gmail.com>
2025-03-13 15:20:32 +0530

    added ValidateFeedCacheUseCaseTests

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -0,0 +1,39 @@
+//
+//  ValidateFeedCacheUseCaseTests.swift
+//  EssentialFeed
+//
+//  Created by Revanth Kausikan on 13/03/25.
+//
+
+import Testing
+import Foundation
+import EssentialFeed
+
+final class ValidateFeedCacheUseCaseTests: EFTesting {
+    @Test("Init doesn't message store upon creation")
+    func init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        #expect(store.receivedMessages == [])
+    }
+}
+
+// MARK: - Helpers
+extension ValidateFeedCacheUseCaseTests {
+    private var anyURL: URL { URL(string: "any-url.com")! }
+    private var anyError: NSError { NSError(domain: "any error", code: 1) }
+    private var uniqueImage: FeedImage { .init(id: UUID(), description: "any", location: "any", url: anyURL) }
+    private typealias CacheFeedUseCaseTestsSUT = (sut: LocalFeedLoader, store: FeedStoreSpy)
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init,
+                         fileID: String = #fileID, filePath: String = #filePath,
+                         line: Int = #line, column: Int = #column) -> CacheFeedUseCaseTestsSUT {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        
+        trackForMemoryLeak(sut, sourceLocation: .init(fileID: fileID, filePath: filePath, line: line, column: column))
+        trackForMemoryLeak(store, sourceLocation: .init(fileID: fileID, filePath: filePath, line: line, column: column))
+        
+        return (sut, store)
+    }
+}
