f4f596d727b2047dc56b65a2382e193c3b9d1022
RevanthKausikan <revanth.k.h@gmail.com>
2025-03-13 19:58:57 +0530

    resued expect method for tests

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -75,15 +75,7 @@ final class CodableFeedStoreTests: EFTesting {
     func retrieve_deliversEmptyCacheOnEmptyStore() async {
         let sut = makeSUT()
         
-        await withCheckedContinuation { continuation in
-            sut.retrieve { result in
-                switch result {
-                case .empty: break
-                default: Issue.record("Expected empty result, got \(result)")
-                }
-                continuation.resume()
-            }
-        }
+        await expect(sut, toRetrieve: .empty)
     }
     
     @Test("Retrieve has no side effect on empty cache")
@@ -104,7 +96,7 @@ final class CodableFeedStoreTests: EFTesting {
     }
     
    @Test("Retrieve after inserting to empty cache delivers inserted values")
-    func retrieve_afterInsertingToEmptyCacheDeliveresInsertedValues() async {
+    func retrieve_afterInsertingToEmptyCache_deliveresInsertedValues() async {
         let sut = makeSUT()
         let feed = getUniqueImageFeed().local
         let timestamp = Date()
@@ -115,18 +107,11 @@ final class CodableFeedStoreTests: EFTesting {
                 case .none: break
                 default: Issue.record("Expected successful insertion, got \(String(describing: insertionError))")
                 }
-                
-                sut.retrieve { retrieveResult in
-                    switch retrieveResult {
-                    case let .found(retrievedFeed, retrievedTimestamp):
-                        #expect(retrievedFeed == feed)
-                        #expect(retrievedTimestamp == timestamp)
-                    default: Issue.record("Expected found result with \(feed) and \(timestamp), got \(retrieveResult) instead.")
-                    }
-                    continuation.resume()
-                }
+                continuation.resume()
             }
         }
+        
+        await expect(sut, toRetrieve: .found(feed: feed, timestamp: timestamp))
     }
     
     @Test("Retrieve has no side effects on non empty cache")
@@ -167,6 +152,26 @@ extension CodableFeedStoreTests {
         let sut = CodableFeedStore(storeURL: testSpecificStoreURL)
         trackForMemoryLeak(sut, sourceLocation: .init(fileID: fileID, filePath: filePath, line: line, column: column))
         return sut
+    }
+    
+    private func expect(_ sut: CodableFeedStore, toRetrieve expectedResult: RetrieveCacheFeedResult,
+                        fileID: String = #fileID, filePath: String = #filePath,
+                        line: Int = #line, column: Int = #column) async {
+        await withCheckedContinuation { continuation in
+            sut.retrieve { receivedResult in
+                switch (receivedResult, expectedResult) {
+                case (.empty, .empty): break
+                case let (.found(retrievedFeed, retrievedTimestamp), .found(expectedFeed, expectedTimestamp)):
+                    #expect(retrievedFeed == expectedFeed, "Expected \(expectedFeed), got \(retrievedFeed)",
+                            sourceLocation: .init(fileID: fileID, filePath: filePath, line: line, column: column))
+                    #expect(retrievedTimestamp == expectedTimestamp, "Expected \(expectedTimestamp), got \(retrievedTimestamp)",
+                            sourceLocation: .init(fileID: fileID, filePath: filePath, line: line, column: column))
+                default: Issue.record("Expected \(expectedResult), got \(receivedResult)",
+                                      sourceLocation: .init(fileID: fileID, filePath: filePath, line: line, column: column))
+                }
+                continuation.resume()
+            }
+        }
     }
     
     private func setupEmptyStoreState() {
