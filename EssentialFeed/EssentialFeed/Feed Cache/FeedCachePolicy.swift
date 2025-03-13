b6a4d89d043df60a4b28b18c11d805412e94f7fb
RevanthKausikan <revanth.k.h@gmail.com>
2025-03-13 17:11:02 +0530

    changing to enum removes init req by having it as struct

--- EssentialFeed/EssentialFeed/Feed Cache/FeedCachePolicy.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -7,11 +7,9 @@
 
 /// This is a value object without identity as opposed to entities (models with identity).
 /// Since its not supposed to change, make it `static` and `private init`.
-struct FeedCachePolicy {
+enum FeedCachePolicy {
     private static let calendar = Calendar(identifier: .gregorian)
     private static let maxCacheAgeInDays = 7
-    
-    private init() {}
     
     static func validate(_ timestamp: Date, against date: Date) -> Bool {
         guard let macCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else { return false }
