36424829f04ea2fb952c90651b5b1240ee9661c4
RevanthKausikan <revanth.k.h@gmail.com>
2025-03-13 17:09:11 +0530

    moved FeedCachePolicy to its own file

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -5,20 +5,6 @@
 //  Created by Revanth Kausikan on 07/03/25.
 //
 
-/// This is a value object without identity as opposed to entities (models with identity).
-/// Since its not supposed to change, make it `static` and `private init`.
-final class FeedCachePolicy {
-    private static let calendar = Calendar(identifier: .gregorian)
-    private static let maxCacheAgeInDays = 7
-    
-    private init() {}
-    
-    static func validate(_ timestamp: Date, against date: Date) -> Bool {
-        guard let macCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else { return false }
-        return date < macCacheAge
-    }
-}
-
 public final class LocalFeedLoader {
     private let store: FeedStore
     private let currentDate: () -> Date
