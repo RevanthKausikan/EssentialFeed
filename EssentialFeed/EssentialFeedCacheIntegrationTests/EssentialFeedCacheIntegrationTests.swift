6f5c07d4726bedd53ef191bf137cf13c3633cd3b
RevanthKausikan <revanth.k.h@gmail.com>
2025-03-15 13:55:05 +0530

    Added EssentialFeedCacheIntegrationTests & converted groups to folders

---
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,15 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Revanth Kausikan on 15/03/25.
+//
+
+import Testing
+
+struct EssentialFeedCacheIntegrationTests {
+
+    @Test func example() {
+        
+    }
+}
