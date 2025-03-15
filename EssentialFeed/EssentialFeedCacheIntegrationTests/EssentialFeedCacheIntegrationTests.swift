8694e7fc00d7890bb71e10ef057ccfc466857729
RevanthKausikan <revanth.k.h@gmail.com>
2025-03-15 14:10:55 +0530

    Load delivers no items on empty cache

--- EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -6,10 +6,44 @@
 //
 
 import Testing
+import EssentialFeed
 
-struct EssentialFeedCacheIntegrationTests {
-
-    @Test func example() {
+final class EssentialFeedCacheIntegrationTests: EFTesting {
+    
+    @Test("Load delivers no items on empty cache")
+    func load_deliversNoItemsOnEmptyCache() async throws {
+        let sut = try makeSUT()
         
+        await withCheckedContinuation { continuation in
+            sut.load { result in
+                switch result {
+                case .success(let imageFeed): #expect(imageFeed.isEmpty)
+                default: Issue.record("Expected empty result, got \(result)")
+                }
+                continuation.resume()
+            }
+        }
+    }
+}
+
+// MARK: - Helpers
+extension EssentialFeedCacheIntegrationTests {
+    private var testSpecificStoreURL: URL {
+        cachesDirectory.appendingPathComponent("\(type(of: self)).store")
+    }
+    
+    private var cachesDirectory: URL {
+        FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
+    
+    private func makeSUT(fileID: String = #fileID, filePath: String = #filePath,
+                         line: Int = #line, column: Int = #column) throws -> LocalFeedLoader {
+        let bundle = Bundle(for: CoreDataFeedStore.self)
+        let storeURL = testSpecificStoreURL
+        let store = try CoreDataFeedStore(storeURL: storeURL, bundle: bundle)
+        let sut = LocalFeedLoader(store: store, currentDate: Date.init)
+        trackForMemoryLeak(sut, sourceLocation: .init(fileID: fileID, filePath: filePath, line: line, column: column))
+        trackForMemoryLeak(store, sourceLocation: .init(fileID: fileID, filePath: filePath, line: line, column: column))
+        return sut
     }
 }
