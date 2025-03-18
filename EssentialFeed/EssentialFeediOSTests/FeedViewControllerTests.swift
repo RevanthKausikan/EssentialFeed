cabb637605c1796a5c2801c5d84914211583555c
RevanthKausikan <revanth.k.h@gmail.com>
2025-03-18 19:59:43 +0530

    ViewDidLoad loads feed

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -6,13 +6,24 @@
 //
 
 import Testing
+import UIKit
 
-final class FeedViewController {
-    init(loader: LoaderSpy) {
+final class FeedViewController: UIViewController {
+    private var loader: LoaderSpy?
+    
+    convenience init(loader: LoaderSpy) {
+        self.init()
+        self.loader = loader
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        loader?.load()
     }
 }
 
+@MainActor
 final class FeedViewControllerTests: EFTesting {
     @Test("Init does not load feed")
     func init_doesNotLoadFeed() {
@@ -20,6 +31,16 @@ final class FeedViewControllerTests: EFTesting {
         _ = FeedViewController(loader: loader)
         
         #expect(loader.loadCallCount == 0)
+    }
+    
+    @Test("ViewDidLoad loads feed")
+    func viewDidLoad_loadsFeed() {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        
+        sut.loadViewIfNeeded()
+        
+        #expect(loader.loadCallCount == 1)
     }
 }
 
