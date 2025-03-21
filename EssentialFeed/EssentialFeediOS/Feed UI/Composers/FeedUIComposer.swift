c0c8832021690644af6a873da38f80a761e61d41
RevanthKausikan <revanth.k.h@gmail.com>
2025-03-21 22:44:31 +0530

    refactored MainQueueDisatchDecorator

--- EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -25,22 +25,26 @@ public final class FeedUIComposer {
     }
 }
 
-fileprivate final class MainQueueDisatchDecorator: FeedLoader {
-    private let decoratee: FeedLoader
+fileprivate final class MainQueueDisatchDecorator<T> {
+    private let decoratee: T
     
-    init(decoratee: FeedLoader) {
+    init(decoratee: T) {
         self.decoratee = decoratee
     }
     
+    func dispatch(completion: @escaping () -> Void) {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async(execute: completion)
+        }
+        
+        completion()
+    }
+}
+    
+extension MainQueueDisatchDecorator: FeedLoader where T == FeedLoader {
     func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        decoratee.load { result in
-            if Thread.isMainThread {
-                completion(result)
-            } else {
-                DispatchQueue.main.async {
-                    completion(result)
-                }
-            }
+        decoratee.load { [weak self] result in
+            self?.dispatch { completion(result) }
         }
     }
 }
